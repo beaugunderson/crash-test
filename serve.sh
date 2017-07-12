0e5efcf3d3702bd61b6b3b845699dbc00d353af8
Beau Gunderson <beau@beaugunderson.com>
2017-07-12 13:59:02 -0700

    try printing

--- serve.sh
+++ serve.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export LD_PRELOAD=./stack-fix.so
+export LD_PRELOAD=/app/stack-fix.so
 
 ./manage.py migrate
 ./manage.py runserver 0.0.0.0:$PORT
--- stack-fix.c
+++ stack-fix.c
@@ -1,10 +1,12 @@
 #include <dlfcn.h>
-
 #include <pthread.h>
+#include <stdio.h>
 
 typedef int (*func_t)(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void*), void *arg);
 
 int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void*), void *arg) {
+	printf("XXX pthread_create override called\n");
+
 	pthread_attr_t local;
 	int used = 0, ret;
 
